a68164f8118a9ddf24f44db20fdeffd150ac1ec9
SidneyAks <Sidneyaks@gmail.com>
2020-09-24 14:46:40 -0500

    Added Nuspec

--- DynamicXml/Properties/AssemblyInfo.cs
+++ DynamicXml/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamicXml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DynamicXml")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sidney Akers")]
 [assembly: AssemblyProduct("DynamicXml")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
